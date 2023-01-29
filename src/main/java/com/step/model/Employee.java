1f8f69685c9e7b7073e4daec01180a7f6a5214b9
sscerbatiuc <s.scerbatiuc@jdi.nl>
2023-01-29 23:17:13 +0200

    Added Employee managed entity.

---
+++ src/main/java/com/step/model/Employee.java
@@ -0,0 +1,68 @@
+package com.step.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employee")
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(length = 70)
+    private String name;
+
+    @Column(length = 70)
+    private String surname;
+
+    @Column
+    private LocalDate birthdate;
+
+    @Column(precision = 6, scale = 2)
+    private Double salary; // 145.32
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
